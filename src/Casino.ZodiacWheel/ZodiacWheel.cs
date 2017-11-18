9f8a3507796948d6f970fc6de16279aa93cf05af
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-11-18 14:13:07 +0800

    Add Zodiac Wheel tools

---
+++ src/Casino.ZodiacWheel/ZodiacWheel.cs
@@ -0,0 +1,14 @@
+﻿using Casino.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.ZodiacWheel
+{
+	public class ZodiacWheel
+	{
+		public Double EvensProbability => (double)(6 * 4 + 1) / 54;
+	}
+}
