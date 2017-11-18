1c81e302ac7f7f460090ea408e76b15749fc6601
Mohammad Chamanpara <mohammad.chamanpara@gmail.com>
2017-11-18 10:28:18 +0800

    Add Core library

---
+++ src/BettingStrategy.Core/IGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingStrategy.Core
+{
+    public interface IGame
+    {
+		/// <summary>
+		/// Runs the game.
+		/// </summary>
+		/// <param name="bankroll">The initial bankroll.</param>
+		/// <param name="spins">The sample set of spins.</param>
+		/// <param name="IStrategy">The strategy.</param>
+		/// <returns>Profit of the game</returns>
+		int Run(int bankroll, List<Outcome> spins, IStrategy strategy);
+    }
+}
---
+++ src/BettingStrategy.Core/IStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingStrategy.Core
+{
+    public interface IStrategy
+    {
+		int NextBet(Outcome lastOutcome, int lastBet, int currentProfit);
+    }
+}
